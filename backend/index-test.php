8d34e7ba66357f891fd464cd37c0a6cacdc715e7
Tuan Nguyen <nganhtuan63@gmail.com>
2012-02-08 16:37:01 +0700

    Edit Test Index File

--- backend/index-test.php
+++ backend/index-test.php
@@ -1,9 +1,9 @@
 <?php
 
 // remove the following lines when in production mode
-//defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
-//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 // Set the constant for the FRONT_STORE Directory
 // Don't change if you are not sure
--- backend/protected/views/besite/index.php
+++ backend/protected/views/besite/index.php
@@ -1,10 +1,42 @@
 <?php $this->pageTitle=SITE_NAME; ?>
-<p style="text-transform: uppercase;"><?php echo t('Quick actions: '); ?></p>
+<p><?php echo t('What do you want to do today?'); ?></p>
+<div>
+<?php $types=GxcHelpers::getAvailableContentType(); ?>
+<ul class="shortcut-buttons-set">
+<?php foreach($types as $type) : ?>
+<li>
+<a href="<?php echo bu().'/beoject/create/type/'.$type['id']; ?>" class="shortcut-button">
+<span>
+<img alt="icon" src="<?php 
+$icon_asset=Yii::app()->assetManager->publish(Yii::getPathOfAlias('common.content_type.'.$type['id'].'.assets'), false, -1, false);                    
+echo $icon_asset.'/'.$type['icon']; ?>"><br />
+<?php echo t('Create').' '.$type['name'];?>
+</span></a></li>
+<?php endforeach; ?>
 
-<p>
-	
-</p>
+<li>
+<a href="<?php echo bu().'/bepage/create'; ?>" class="shortcut-button">
+<span>
+<img alt="icon" src="<?php 
+                    
+echo $this->backend_asset.'/images/paper.png'; ?>"><br />
+<?php echo t('Create new Page');?>
+</span></a></li>
 
-<p><?php echo t('This CMS is powered by ');?><a href="http://www.gxcsolutions.com">GXC Solutions</a>.
-<?php echo t('Having problems? Contact us at: ')?> <a href="mailto:cms-support@gxcsolutions.com">cms-support@gxcsolutions.com</a>,
-<?php echo t('or Join us at');?><a href="https://github.com/nganhtuan63/GXC-CMS">Github</a>,</p>
+
+<li>
+<a href="<?php echo bu().'/beresource/create'; ?>" class="shortcut-button">
+<span>
+<img alt="icon" src="<?php                     
+echo $this->backend_asset.'/images/upload_file.png'; ?>"><br />
+<?php echo t('Upload a File');?>
+</span></a></li>
+
+
+</ul>
+<div style="clear:both"></div>
+</div>
+
+<p><?php echo t('This CMS is an Open Source CMS, powered by ');?><a href="http://www.gxcsolutions.com">GXC Solutions</a>.</p>
+<p><?php echo t('Having problems? Contact us at: ')?> <a href="mailto:cms-support@gxcsolutions.com">cms-support@gxcsolutions.com</a>,
+<?php echo t('or Join us at');?><a href="https://github.com/nganhtuan63/GXC-CMS">Github</a></p>
