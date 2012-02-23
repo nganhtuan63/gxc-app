919f0e7afb62c676f956a20b3547c8fec92aab66
Quyen Nguyen Tuan <nguyen.tuan.quyen.it@gmail.com>
2012-02-23 20:37:03 +0700

    Reupdate common/config.php to the version in master branch

--- common/config.php
+++ common/config.php
@@ -94,7 +94,7 @@ return array(
 	                'schemaCachingDuration' => 3600,
 	                'emulatePrepare' => true,
 	                'username' => 'root',
-	                'password' => '',
+	                'password' => 'root',
 	                'charset' => 'utf8',
 	                'tablePrefix' => 'gxc_'
 	            ),
