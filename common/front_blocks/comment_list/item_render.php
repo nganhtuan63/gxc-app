9ee150735be0e15101179c4975297eaeeeea7500
Nguyen Tuan Quyen <nguyen.tuan.quyen.it@gmail.com>
2012-03-13 00:29:36 +0700

    Comment reply

--- common/front_blocks/comment_list/item_render.php
+++ common/front_blocks/comment_list/item_render.php
@@ -1,4 +1,4 @@
-<div class="wide">
+<div class="wide" id="comment">
 	<p style="margin-top:0">
 		<strong><?php echo $data->author_name. '  -  ' . $data->topic;
 		 ?></strong>
@@ -6,4 +6,25 @@
 		<br />
 		<?php echo $data->content; ?>
 	</p>
+
+	<div class="row button">
+		<?php 
+		//Reply button for each comment : 
+		//when button clicked, the field content of comment form will be focused
+		//and pre-filled by the text : @ + [author_name being replied]
+			$this->widget('zii.widgets.jui.CJuiButton', array(
+  			'id'=>'btReply'.$data->comment_id,
+  			'buttonType'=>'submit',
+ 			'name'=>$data->author_name,
+  			'caption'=>'Reply',
+ 			'options'=>array('icons'=>'js:{primary:"ui-icon-wrench"}'),
+  			'onclick'=>'js:function(){
+					var name = $(this).attr("name");
+					$("#Comment_content").focus();
+					$("#Comment_content").val("@"+name+":\n");
+					return false;
+					}',
+ 			));
+		?>
+	</div>
 </div>
