2f8ae116e2475d329a91f551aa12bf02283c1064
Nguyen Tuan Quyen <nguyen.tuan.quyen.it@gmail.com>
2012-03-12 21:34:20 +0700

    Manage comments list given by the object_id

--- common/front_blocks/comment_list/comment_list_block_output.php
+++ common/front_blocks/comment_list/comment_list_block_output.php
@@ -1,10 +1,14 @@
+<!-- 
+- Display the comment list of the given object
+- Display a form that allow users to give their comment -->
+
 <?php 
-	//So I get the first content list and want to return as a Data Provider
-    //$content_list_data_provider = ListViewBlock::getContentList($this->content_list[0], null , null, ConstantDefine::CONTENT_LIST_RETURN_DATA_PROVIDER);
+	//Get the comment list given by the object_id
 	$object_id=(int)$_GET['id'];
-    $comment_list_data_provider = CommentListBlock::getCommentList($object_id);		    
-	//Get the current content List model
-    if (isset($comment_list_data_provider) && $comment_list_data_provider != null) : ?>
+    $comment_list_data_provider = CommentListBlock::getCommentList($object_id);	
+    	    
+	//Display the list if its itemCount > 0
+    if (isset($comment_list_data_provider) && $comment_list_data_provider != null && $comment_list_data_provider->getItemCount(false) > 0) : ?>
         <h2><?php echo t('Comments'); ?></h2>
             <?php 
             	$this->widget('zii.widgets.CListView',
@@ -22,4 +26,10 @@
             			);   
                             							                                                       
             ?>
-	<?php endif; ?>       
+	<?php endif; ?> 
+	      
+<?php if(isset($comment_list_data_provider) && $comment_list_data_provider != null): ?>
+	<h2><?php echo t('Enter a comment'); ?></h2>
+		<?php $this->widget('ModelCreateWidget', array('model_name'=>'Comment',));?>
+		
+<?php endif;?>	
