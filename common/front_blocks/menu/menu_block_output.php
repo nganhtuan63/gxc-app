bb9ebd449f92c2010edb49a4b20ed9559412fb2c
Tuan Nguyen <nganhtuan63@gmail.com>
2012-03-15 16:02:04 +0700

    Implement cache for the menu render

--- common/front_blocks/menu/menu_block_output.php
+++ common/front_blocks/menu/menu_block_output.php
@@ -1,4 +1,5 @@
 <?php if (isset($menus)):?>
+	<?php if($this->beginCache('menu-render-'.$this->menu_id, array('duration'=>7200))) { ?>
       <div id="navigation" class="jqueryslidemenu">
             <ul class="menu_level_1">      	
 	        	<?php foreach ($menus as $menu) :?>	
@@ -8,7 +9,17 @@
 						if(count($second_level_menus)>0) : ?>
 						<ul class="menu_level_2">
 							<?php foreach ($second_level_menus as $second_menu) : ?>
-									<li><a href="<?php echo $second_menu['link'];?>"><?php echo $second_menu['name'];?></a></li>
+									<li><a href="<?php echo $second_menu['link'];?>"><?php echo $second_menu['name'];?></a>										
+										<?php 
+					          			$third_level_menus=MenuBlock::getMenuItems($second_menu['id'],$this->menu_id);
+										if(count($third_level_menus)>0) : ?>
+										<ul class="menu_level_3">
+											<?php foreach ($third_level_menus as $third_menu) : ?>
+												<li><a href="<?php echo $third_menu['link'];?>"><?php echo $third_menu['name'];?></a></li>
+											<?php endforeach; ?>
+										</ul>
+										<?php endif; ?>
+									</li>
 							<?php endforeach; ?>
 						</ul>
 						<?php endif; ?>	
@@ -17,4 +28,5 @@
 	        </ul>
       <br style="clear: left">
       </div>      
+   <?php $this->endCache(); } ?>
 <?php endif;?>
